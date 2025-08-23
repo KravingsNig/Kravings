6c749f4b7e2dd36fd27b64c48523c36df7fdabad
Kravings Nigeria <kravingsnigeria@gmail.com>
2025-08-23 10:50:46 +0000

    lets remove the *K* logo and use *nice cart design icon* instead

--- src/components/kravings-logo.tsx
+++ src/components/kravings-logo.tsx
@@ -1,20 +1,6 @@
+import { ShoppingCart } from "lucide-react";
 import type { SVGProps } from "react";
 
 export function KravingsLogo(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="text-primary"
-      {...props}
-    >
-      <path
-        d="M11 2L3 10V22H9V14H11L18 22L21 19L14.5 12.5L21 6L18 3L13 8L11 6V2Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
+  return <ShoppingCart {...props} />;
 }
